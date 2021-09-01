e3ffbc11d435c4b5ce9f3c59ea67853605170e04
Marika <mashakonturova@gmail.com>
2021-09-01 13:13:14 +0300

    add default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, withRouter} from "react-router-dom";
+import {Switch, Route, withRouter, Redirect} from "react-router-dom";
 import {Pokedex} from "./Components/Pokedex";
 import {Pokemon} from "./Components/Pokemon";
 
@@ -8,12 +8,13 @@ function App(props: any) {
     return (
         <div className="App">
             <Switch>
-                <Route exact path={'/pokedex'} render={() => <Pokedex />}/>
-                <Route exact path={'/pokemon/:pokemonId'} render={() => <Pokemon />}/>
+                <Route exact path="/" render={() => <Redirect to="/pokedex"/>}/>
+                <Route exact path={'/pokedex'} render={() => <Pokedex/>}/>
+                <Route exact path={'/pokemon/:pokemonId'} render={() => <Pokemon/>}/>
             </Switch>
         </div>
     );
 }
 
 export default withRouter(App);
-// export default App;
+
